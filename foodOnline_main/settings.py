9b5a1d50399d38ebe5ef70aade60b29d747f3b18
Prasun Baranwal <prasubaran@gmail.com>
2025-01-11 01:38:24 +0530

    Update settings.py

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -155,7 +155,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='prasubaran@gmail.com'
-EMAIL_HOST_PASSWORD='hxtt sily tkht legt'
+EMAIL_HOST_PASSWORD=''
 
 EMAIL_USE_TLS= True
 DEFAULT_FROM_EMAIL='foodOnline Marketplace <prasubaran@gmail.com>'
